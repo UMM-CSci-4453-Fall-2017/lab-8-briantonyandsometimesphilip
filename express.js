e8d87a70ac060ffed207473da3acd9abe19c3b0d
Tony Song <songx823@morris.umn.edu>
2017-11-07 17:33:06 -0600

    Refactor express.js

--- express.js
+++ express.js
@@ -24,7 +24,8 @@ app.get("/user", function(req,res) {
 // return JSON object of current buttons
 // modify values in till_buttons table (if you want to be fancy... NOT REQUIRED)
 app.get("/buttons",function(req,res){
-  var dbf = getDatabases(DBF)
+  var sql = "SELECT * from Tony.till_buttons";
+  var dbf = queryPromiser(DBF, sql)
   .then(function (results) {
     res.send(results);
   })
@@ -32,35 +33,33 @@ app.get("/buttons",function(req,res){
   .catch(function(err){console.log("DANGER:",err)});
 });
 
-var getDatabases=function(db){//Returns a promise that can take a handler ready to process the results
-  var sql = "SELECT * from Tony.till_buttons";
-  db.generateConnection();
-  return db.query(mysql.format(sql)); //Return a promise
-}
-
 // update the current transaction table to reflect the-item button clicked
 app.get("/click",function(req,res){
   // buttonID
   var btnID = req.param('id');
   var itemInfo = null;
+  var itemInfoSql = 'select invID, label, price from Tony.till_buttons where buttonID = ' + btnID;
 
-  getItemInfo(DBF, btnID)
+  queryPromiser(DBF, itemInfoSql)
   .then(function (idResult) {
     itemInfo = idResult[0];
-    console.log(itemInfo);
-    return isValid(DBF, itemInfo.invID);
-    })
+    var validSql = 'select exists (select invID from Tony.current_trans where invID = ' + itemInfo.invID + ') as isValid';
+    return queryPromiser(DBF, validSql);
+  })
   .then(function (existResult) {
     if (existResult[0].isValid) {
       // Increase amount by 1
-      return increaseItemAmount(DBF, itemInfo);
+      var updateSql = 'UPDATE Tony.current_trans SET amount = amount + 1 WHERE invID = ' + itemInfo.invID;
+      return queryPromiser(DBF, updateSql);
     } else {
       // Create a new one
-      return createItemRow(DBF, itemInfo);
+      var insertSql = 'insert into Tony.current_trans values (' + itemInfo.invID + ',' + 1 + ',\"' + itemInfo.label + '\",' + itemInfo.price + ')';
+      return queryPromiser(DBF, insertSql);
     }
   })
   .then(function (dummy) {
-    return transaction(DBF);
+    var showSql = 'select * from Tony.current_trans';
+    return queryPromiser(DBF, showSql);
   })
   .then(function (currentTransaction) {
     res.send(currentTransaction);
@@ -68,37 +67,6 @@ app.get("/click",function(req,res){
   .then(DBF.releaseDBF)
   .catch(function(err){console.log("DANGER:",err)});
 });
-
-function getItemInfo(setup , btnID) {
-  var sql = 'select invID, label, price from Tony.till_buttons where buttonID = ' + btnID;
-  setup.generateConnection();
-  return setup.query(mysql.format(sql)); // Return a promise
-}
-
-function isValid(setup, invID) {
-  var sql = 'select exists (select invID from Tony.current_trans where invID = ' + invID + ') as isValid';
-  setup.generateConnection();
-  return setup.query(mysql.format(sql)); // Return a promise
-}
-
-function increaseItemAmount(setup, itemInfo) {
-  var sql = 'UPDATE Tony.current_trans SET amount = amount + 1 WHERE invID = ' + itemInfo.invID;
-  setup.generateConnection();
-  return setup.query(mysql.format(sql)); // Return a promise
-}
-
-function createItemRow(setup, itemInfo) {
-  var sql = 'insert into Tony.current_trans values (' + itemInfo.invID + ',' + 1 + ',\"' + itemInfo.label + '\",' + itemInfo.price + ')';
-  setup.generateConnection();
-  return setup.query(mysql.format(sql)); // Return a promise
-}
-
-function transaction(setup) {
-  var sql = 'select * from Tony.current_trans';
-  setup.generateConnection();
-  return setup.query(mysql.format(sql)); // Return a promise
-}
-
 
 // TODO for lab 9
 // complete the current transaction and clear the transaction table
@@ -114,31 +82,34 @@ app.get("/void", function(req,res) {
 
 // provide JSON object of items in current transaction
 app.get("/list", function(req,res) {
-
 });
 
 // remove item(s) from current transaction
 app.get("/delete", function(req,res) {
-    // invID
-    var invID = req.param('id');
+  // invID
+  var invID = req.param('id');
+  var sql = 'Delete from Tony.current_trans where invID = ' + invID;
 
-    deleteItem(DBF, invID)
-        .then(function (currentTransaction) {
-            return transaction(DBF);
-        })
-        .then (function (currentTransaction) {
-            res.send(currentTransaction);
-            return currentTransaction;
-        })
-        .then(DBF.releaseDBF)
-        .catch(function(err){console.log("DANGER:",err)});
+  queryPromiser(DBF, sql)
+  .then(function (currentTransaction) {
+    var showSql = 'select * from Tony.current_trans';
+    return queryPromiser(DBF, showSql);
+  })
+  .then (function (currentTransaction) {
+    res.send(currentTransaction);
+    return currentTransaction;
+  })
+  .then(DBF.releaseDBF)
+  .catch(function(err){console.log("DANGER:",err)});
 
 });
 
-function deleteItem(setup, invID) {
-    var sql = 'Delete from Tony.current_trans where invID = ' + invID;
-    setup.generateConnection();
-    return setup.query(mysql.format(sql)); // Return a promise
+//Returns a promise that can take a handler ready to process the results
+// Takes the imported db-setup and sql query string, and
+// returns the result of the query as promise.
+function queryPromiser(setup, sqlQuery) {
+  setup.generateConnection();
+  return setup.query(mysql.format(sqlQuery)); // Return a promise
 }
 
 app.listen(port);
